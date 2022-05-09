539c8ceab545a7f3cee7ce12b4e03952057e2bb7
awaisanwar544 <awaisanwar544@gmail.com>
2022-05-10 03:27:02 +0500

    Add class person student teacher

---
+++ student.rb
@@ -0,0 +1,14 @@
+require './person'
+# Student class
+class Student < Person
+  attr_reader :classroom
+
+  def initialize(age, name, parent_permission, classroom)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
