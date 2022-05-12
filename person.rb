754c87b2e760097348cb2717eee7053e27ea2697
awaisanwar544 <awaisanwar544@gmail.com>
2022-05-12 14:04:23 +0500

    Add add_rental method to book and person class

--- person.rb
+++ person.rb
@@ -2,8 +2,8 @@ require './nameable'
 
 # Person class
 class Person < Nameable
-  attr_reader :id, :rental
-  attr_accessor :name, :age
+  attr_reader :id
+  attr_accessor :name, :age, :rental
 
   def initialize(age, name = 'unknown', parent_permission: true)
     super
@@ -16,6 +16,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 
   private
